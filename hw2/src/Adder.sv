eab8fe3d052091a927a91661b6b7104f96b649d7
p76131416 <P76131416@gs.ncku.edu.tw>
2024-09-10 21:11:28 +0800

    test

--- hw2/src/Adder.sv
+++ hw2/src/Adder.sv
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/Adder.sv
 module Adder (
     input [31:0] in1,
     input [31:0] in2,
@@ -10,17 +9,4 @@ always_comb begin
     out = in1 + in2;
 end
 
-=======
-module Adder (
-    input [31:0] in1,
-    input [31:0] in2,
-
-    output logic [31:0] out
-);
-
-always_comb begin
-    out <= in1 + in2;
-end
-
->>>>>>> e96df0076629e4a3b86613aa9ce64749610c4064:hw1/src/Adder.sv
 endmodule
